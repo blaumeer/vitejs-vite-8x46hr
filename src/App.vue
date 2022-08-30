d9efccbaaf872793dce4fa2838eb054d6d7c2b92
blaumeer <156816764@qq.com>
2022-08-30 16:07:54 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue';
+// import { Lightning } from '@icon-park/vue-next';
+import '@icon-park/vue-next/styles/index.css';
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <check-one theme="filled" size="32" fill="#17db08" />
+    <lightning theme="filled" size="24" fill="#bd10e0" :strokeWidth="3" />
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
